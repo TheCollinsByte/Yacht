c9bfd9f5645218e66ae824b6fc67046809578dd8
colspan <collin@collindigm.com>
2023-03-19 14:55:01 +0300

    Test Set-Up: YatchTest Class

---
+++ src/test/java/YatchTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class YatchTest {
+
+    @Test
+    public void shouldPass() throws Exception {
+        assertThat(true)
+                .isTrue();
+    }
+
+}
