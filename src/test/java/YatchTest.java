e0bc1250076386329b3722556760d9b0fb1430db
colspan <collin@collindigm.com>
2023-03-19 16:24:40 +0300

    Test initialization NOT working

--- src/test/java/YatchTest.java
+++ src/test/java/YatchTest.java
@@ -1,12 +1,12 @@
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class YatchTest {
 
     @Test
     public void shouldPass() throws Exception {
-        assertThat(true)
+        assertThat(false)
                 .isTrue();
     }
 
